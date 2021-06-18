253ef377160da5dbb35a60bcc9fb0565344e2a1f
ritsuki990907 <ritsuki990907@gmail.com>
2021-06-18 23:05:11 +0900

    現在地取得コードの分析 #1

--- app/src/main/java/com/example/walky/MapsActivity.kt
+++ app/src/main/java/com/example/walky/MapsActivity.kt
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -15,14 +14,12 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.walky.databinding.ActivityMapsBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.Marker
-/*GoogleMap.OnMarkerClickListener 追加*/
+
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
-    /*追加*/
     private lateinit var mMap: GoogleMap
     private lateinit var lastLocation: Location
     private lateinit var fusedLocationClient: FusedLocationProviderClient
@@ -30,7 +27,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     companion object {
         private  const val LOCATION_REQUEST_CODE = 1
     }
-    /*ここまで*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
@@ -38,29 +35,29 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        /*追加*/
-       fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        /*ここまで*/
+
+        /**
+         * 削除したら動作しなかった
+         * onMapReady->fusedLocationClientの順に実行
+         */
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        /*  追加 */
-
         mMap.uiSettings.isZoomControlsEnabled = true
         mMap.setOnMarkerClickListener(this)
 
-
         setupMap()
-        /*ここまで*/
-        // Add a marker in Sydney and move the camera
-        //val sydney = LatLng(-34.0, 151.0)
-        //mMap.addMarker(MarkerOptions().position(sydney).title("マーカー"))
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
-    /*  追加  */
+
     private fun setupMap(){
+
+        /**
+         * mMap.isMyLocationEnabled = true
+         * ここに記述しても動作
+         */
 
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -85,13 +82,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         }
     }
 
+    /**
+     * マーカーを表示する用の関数
+     */
     private fun placeMarkerOnMap(currentLatLong: LatLng) {
         val markerOptions = MarkerOptions().position(currentLatLong)
-        markerOptions.title("$currentLatLong")
+        markerOptions.title("debug")
         mMap.addMarker(markerOptions)
-
     }
 
     override fun onMarkerClick(p0: Marker) = false
     }
-    /*ここ*/
