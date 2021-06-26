b9987614b067081c93b789e7e8e814def9f3e7d0
chamberskaz0301 <76543076+chamberskaz0301@users.noreply.github.com>
2021-06-26 10:19:41 +0900

    ボトムボタン　修正第1弾#31

--- app/src/main/java/com/example/walkly/MapsActivity.kt
+++ app/src/main/java/com/example/walkly/MapsActivity.kt
@@ -1,11 +1,13 @@
+
 package com.example.walkly
+
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.walkly.application.MapApplicationService
 import com.example.walkly.ui.MapCallback
-
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MapsActivity : AppCompatActivity() {
 
@@ -17,6 +19,8 @@ class MapsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+
         /**
          * TODO: GPSクラス修正
          *  - 許可しない時の処理
@@ -26,6 +30,8 @@ class MapsActivity : AppCompatActivity() {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(mapCallback)
+
+
     }
 
 }
