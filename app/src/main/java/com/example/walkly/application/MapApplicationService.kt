c78e761f839f6618aa24b52bd7736624466d3d67
ritsuki990907 <ritsuki990907@gmail.com>
2021-06-29 12:52:19 +0900

    仮のルート表示 #45

--- app/src/main/java/com/example/walkly/application/MapApplicationService.kt
+++ app/src/main/java/com/example/walkly/application/MapApplicationService.kt
@@ -1,9 +1,18 @@
 package com.example.walkly.application
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.walkly.domain.model.GPS
 import com.example.walkly.domain.model.MyMap
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.PolyUtil
+import org.json.JSONObject
 
 /**
  * 現在地の取得やアクティビティの開始などの指示を統括している
@@ -21,6 +30,28 @@ class MapApplicationService(private val activity: AppCompatActivity) {
         val gps = GPS(activity)
         gps.enableCurrentLocation(mMap)
         myMap = MyMap(mMap)
+
+
+        val path: MutableList<List<LatLng>> = ArrayList()
+        val urlDirections = "https://maps.googleapis.com/maps/api/directions/json?origin=10.3181466,123.9029382&destination=10.311795,123.915864&key=APIKEEEEEEEEEEEEEEEEEEEEEEY"
+        val directionsRequest = object : StringRequest(Request.Method.GET, urlDirections, Response.Listener {
+                response ->
+            val jsonResponse = JSONObject(response)
+            // Get routes
+            val routes = jsonResponse.getJSONArray("routes")
+            val legs = routes.getJSONObject(0).getJSONArray("legs")
+            val steps = legs.getJSONObject(0).getJSONArray("steps")
+            for (i in 0 until steps.length()) {
+                val points = steps.getJSONObject(i).getJSONObject("polyline").getString("points")
+                path.add(PolyUtil.decode(points))
+            }
+            for (i in 0 until path.size) {
+                mMap.addPolyline(PolylineOptions().addAll(path[i]).color(Color.RED))
+            }
+        }, Response.ErrorListener {
+        }){}
+        val requestQueue = Volley.newRequestQueue(activity)
+        requestQueue.add(directionsRequest)
     }
 
 }
