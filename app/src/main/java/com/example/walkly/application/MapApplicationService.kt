1110c4f9d49dc7cba8d5b76efdbb9ec81196045f
ritsuki990907 <ritsuki990907@gmail.com>
2021-07-04 18:43:34 +0900

    天気予報と周辺施設取得の仮コード

--- app/src/main/java/com/example/walkly/application/MapApplicationService.kt
+++ app/src/main/java/com/example/walkly/application/MapApplicationService.kt
@@ -1,15 +1,21 @@
 package com.example.walkly.application
 
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.example.walkly.R
 import com.example.walkly.domain.model.GPS
+import com.example.walkly.domain.model.MyApplication
 import com.example.walkly.domain.model.mymap.MyMap
 import com.example.walkly.domain.model.mymap.Route
+import com.example.walkly.lib.HTTPRequest
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PointOfInterest
+import org.json.JSONObject
 
 /**
  * 現在地の取得やアクティビティの開始などの指示を統括している
@@ -28,6 +34,43 @@ class MapApplicationService(private val activity: AppCompatActivity) {
         gps.enableCurrentLocation(mMap)
         myMap = MyMap(mMap)
         route = Route(mMap)
+
+
+        // 天気予報
+        if (false) {
+            val url =
+                "http://api.openweathermap.org/data/2.5/weather?lat=35.156065&lon=136.916202&APPID=c0017427d17d19299b0b73042052f372"
+            val listener = Response.Listener<String> { response ->
+                Log.d("debug", response)
+                val jsonResponse = JSONObject(response) // 文字列をJSON形式に変換
+                val weather = jsonResponse.getJSONArray("weather") // weather配列取得
+                val item = weather.getJSONObject(0) // オブジェクト取得
+                Log.d("weather", item.getString("main"))
+                Log.d("icon", item.getString("icon"))
+            }
+            val errorListener = Response.ErrorListener { }
+            HTTPRequest().getRequest(url, listener, errorListener)
+        }
+
+        // 周辺施設
+        if (false) {
+            val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=35.1681694,136.8857641&radius=5000&type=restaurant&key=${MyApplication.getContext().getString(
+                R.string.google_maps_key
+            )}"
+            val listener = Response.Listener<String> { response ->
+                val jsonResponse = JSONObject(response) // 文字列をJSON形式に変換
+                val results = jsonResponse.getJSONArray("results") // results:配列取得
+                for (i in 0 until results.length()) {
+                    val item = results.getJSONObject(i) // i:オブジェクト取得
+                    val geometry = item.getJSONObject("geometry") // geometry:オブジェクト取得
+                    val location = geometry.getJSONObject("location") // location:オブジェクト取得
+                    Log.d("lat", location.getInt("lat").toString()) // 数値のlat:を文字列に変換して表示 実際の利用時は変換不要
+                    Log.d("lng", location.getInt("lng").toString()) // 数値のlng:を文字列に変換して表示 実際の利用時は変換不要
+                }
+            }
+            val errorListener = Response.ErrorListener {  }
+            HTTPRequest().getRequest(url, listener, errorListener)
+        }
     }
 
     /**
