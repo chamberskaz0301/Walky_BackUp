705021cf4d64c8673a2c9a482489636e931bf7d7
Ikema19 <masa0208yuki@icloud.com>
2021-07-05 23:44:08 +0900

    天候予測 完成？#32

--- app/src/main/java/com/example/walkly/domain/model/Weather.kt
+++ app/src/main/java/com/example/walkly/domain/model/Weather.kt
@@ -6,106 +6,77 @@ package com.example.walkly.domain.model
 */
 
 
+import android.util.Log
+import com.android.volley.Response
 import com.example.walkly.lib.HTTPRequest
 import com.google.android.gms.maps.model.LatLng
 import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.URL
-import java.util.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 
-/*変数の初期化*/
-var weatherName = ""
 
+open class Weather() {
 
-class Weather() {
-
-    /*
-    * 天候予測の実行、取得
-    * APIキー(c0017427d17d19299b0b73042052f372)
-    *
-    * AndroidManifest.xmlに
-    * <uses-permission android:name="android.permission.INTERNET" />
-    * <application
-    * android:usesCleartextTraffic="true"
-    * を記述する必要あり？
-    */
     public fun start(latLng: LatLng) {
         println("処理を開始")
 
-        /*CoroutineScope*/
-        CoroutineScope(Dispatchers.Main).launch {
-            /*APIキー*/
-            val APIKEY = "c0017427d17d19299b0b73042052f372"
+        /*変数の設定*/
+        var item = ""
+        var weathericon = ""
+        var weathername = ""
 
-            /*緯度経度の設定*/
-            var placeLat = latLng.latitude
-            var placeLon = latLng.longitude
+        /*HTTPRequest01*/
+        /*APIキー*/
+        val APIKEY = "c0017427d17d19299b0b73042052f372"
 
-            /*接続先URL*/
-            var APIurl = "http://api.openweathermap.org/data/2.5/weather?" +
-                    "lat=" + placeLat + "&" +
-                    "lon=" + placeLon + "&" +
-                    "&APPID=" + APIKEY
+        /*緯度経度の設定*/
+        var placeLat = latLng.latitude
+        var placeLon = latLng.longitude
 
-            //http://api.openweathermap.org/data/2.5/weather?lat=&lon=&APPID=c0017427d17d19299b0b73042052f372
-            //名古屋駅 http://api.openweathermap.org/data/2.5/weather?lat=35.156065&lon=136.916202&APPID=c0017427d17d19299b0b73042052f372
+        /*接続先URL*/
+        var APIurl = "http://api.openweathermap.org/data/2.5/weather?" +
+                "lat=" + placeLat + "&" +
+                "lon=" + placeLon + "&" +
+                "&APPID=" + APIKEY
 
-            var url = URL(APIurl)
+        var url = APIurl
 
-            /*APIから情報を取得する*/
+        /*HTTPRequest02*/
+        val listener = Response.Listener<String> { response ->
+            Log.d("debug", response)
+            val jsonResponse = JSONObject(response)
+            val weather = jsonResponse.getJSONArray("weather")
+            val item = weather.getJSONObject(0)
 
-            var br = BufferedReader(InputStreamReader(url.openStream()))
+            var weathername = item.getString("main")
+            var weathericon = item.getString("icon")
 
-            var errorListener = ""
+            Log.d("weather", item.getString("main"))
+            Log.d("icon", item.getString("icon"))
 
-            HTTPRequest().getRequest(APIurl, br, errorListener)
+            /*取得した文字データを画像に変換*/
+            println("天気を画像データに変換")
+            println(weathericon)
 
+            var iconurl : String
 
-            /*所得した情報を文字列化*/
-            var str = br.readText()
+            if (weathericon == "") {
+                iconurl = "[画像データなし].png"
+            }else {
+                iconurl = weathericon + ".png"
+            }
 
-            /*json形式のデータとして識別*/
-            var json = JSONObject(str)
-
-            /*weatherキーに対応するvalueを表示*/
-            var weatherName = json.get("weather")
-
-            println(weatherName)
-
-
-            createWeatherImage(json)
+            println(iconurl)
         }
 
+        /*HTTPRequest03*/
+        val errorListener = Response.ErrorListener {  }
+
+        HTTPRequest().getRequest(url, listener, errorListener)
+
+
+
     }
+
+
 }
 
-        /*
-        * コピペ
-        * https://note.com/c_omachi/n/nef100a0a23ad
-        * */
-
-
-
-    /*
-    * 取得した文字データを画像に変換
-    */
-    fun createWeatherImage(json: Any) {
-        println("天気を画像データに変換")
-        var weathericon = weatherName
-        var iconurl : String
-
-        if (weathericon == "") {
-            iconurl = "[画像データなし].png"
-        }else {
-            iconurl = weathericon + ".png"
-        }
-
-        println(iconurl)
-        /*画面上に画像を表示するスペースを作っておき、iconurlの値によって、動的に画像の種類が変わるようにする。*/
-
-    }
